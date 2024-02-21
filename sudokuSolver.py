ab5d5bb698cb77c45b77014cc382fa0848e61ff8
tdregmans <tdregmans+github@protonmail.com>
2024-02-21 19:50:28 +0100

    Add comments to SudokuSolver methods

--- sudokuSolver.py
+++ sudokuSolver.py
@@ -13,8 +13,10 @@ SIZE = 9
 DEBUG_MODE = True
 REGULAR_MODE = False
 
-# SudokuSolver object, that solves the sudoku
 class SudokuSolver:
+    """
+        Solves the Sudoku
+    """
     def __init__(self, sudoku, size = SIZE, mode = REGULAR_MODE):
         self.sudoku = sudoku
         self.size = size
@@ -26,6 +28,9 @@ class SudokuSolver:
                 self.sudoku[x] = (list(range(1, self.size + 1)))
 
     def print(self):
+        """
+            Print the Sudoku with the current known fields
+        """
         for x in range(self.size ** 2):
             if len(self.sudoku[x]) == 1:
                 print(str(self.sudoku[x][0]) + "   ", end='')
@@ -35,6 +40,9 @@ class SudokuSolver:
                 print()
 
     def isSolved(self):
+        """
+            Returns True, if Sudoku is solved; else return False
+        """
         for x in range(self.size ** 2):
             if len(self.sudoku[x]) != 1:
                 return False
@@ -42,71 +50,108 @@ class SudokuSolver:
 
 
     def __row(self, location):
-        # return the id of the row that location is located in
+        """
+            (Private method) Return the id of the row that location is located in
+        """
         return location // self.size
 
     def __col(self, location):
-        # return the id of the col that location is located in
+        """
+            (Private method) Return the id of the col that location is located in
+        """
         return location % self.size
 
     def __sub(self, location):
-        # return the id of the subGrid that location is located in
+        """
+            (Private method) Return the id of the subGrid that location is located in
+        """
         return (self.__row(location) // 3) * 3 + (self.__col(location) // 3)
 
     def __rowLocations(self, id):
-        # return a list with all locations of the row with the specified id
+        """
+            (Private method) Return a list with all locations of the row with the specified id
+        """
         return list(range(id * self.size, id * self.size + self.size))
 
     def __colLocations(self, id):
-        # return a list with all location of the columns with the specified id
+        """
+            (Private method) Return a list with all locations of the column with the specified id
+        """
         return list(range(id, self.size * self.size, self.size))
 
     def __subLocations(self, id):
+        """
+            (Private method) Return a list with all locations of the subgrid with the specified id
+        """
         firstFieldInSub = ((id // 3) * 3) * 9 + ((id % 3) * 3)
         return [firstFieldInSub, firstFieldInSub + 1, firstFieldInSub + 2,
                 firstFieldInSub + 9, firstFieldInSub + 10, firstFieldInSub + 11,
                 firstFieldInSub + 18, firstFieldInSub + 19, firstFieldInSub + 20]
 
     def __removePossiblityRow(self, location, possibility):
+        """
+            (Private method) Eliminate the values that are no longer possibilities in row
+        """
         for x in self.__rowLocations(self.__row(location)):
             if possibility in self.sudoku[x] and len(self.sudoku[x]) != 1:
                 self.sudoku[x].remove(possibility)
 
     def __removePossiblityCol(self, location, possibility):
+        """
+            (Private method) Eliminate the values that are no longer possibilities in column
+        """
         for x in self.__colLocations(self.__col(location)):
             if possibility in self.sudoku[x] and len(self.sudoku[x]) != 1:
                 self.sudoku[x].remove(possibility)
 
     def __removePossiblitySub(self, location, possibility):
+        """
+            (Private method) Eliminate the values that are no longer possibilities in subgrid
+        """
         for x in self.__subLocations(self.__sub(location)):
             if possibility in self.sudoku[x] and len(self.sudoku[x]) != 1:
                 self.sudoku[x].remove(possibility)
                 
 
     def __removePossiblity(self, location, possibility):
+        """
+            (Private method) Eliminate the values that are no longer possibilities in the rows, columns
+        """
         self.__removePossiblityRow(location, possibility)
         self.__removePossiblityCol(location, possibility)
         self.__removePossiblitySub(location, possibility)
 
     def __assignInRow(self, location, possibility):
+        """
+            (Private method) Assign the possibility to the only location in the row
+        """
         for x in self.__rowLocations(self.__row(location)):
             if possibility in self.sudoku[location] and len(self.sudoku[x]) != 1:
                 self.sudoku[x] = [possibility]
                 return
             
     def __assignInCol(self, location, possibility):
+        """
+            (Private method) Assign the possibility to the only location in the column
+        """
         for x in self.__colLocations(self.__col(location)):
             if possibility in self.sudoku[location] and len(self.sudoku[x]) != 1:
                 self.sudoku[x] = [possibility]
                 return
             
     def __assignInSub(self, location, possibility):
+        """
+            (Private method) Assign the possibility to the only location in the subgrid
+        """
         for x in self.__subLocations(self.__sub(location)):
             if possibility in self.sudoku[location] and len(self.sudoku[x]) != 1:
                 self.sudoku[x] = [possibility]
                 return
 
     def __assignRow(self, location):
+        """
+            (Private method) Assign all possibilities to the location in all rows
+        """
         sumRow = []
         for x in self.__rowLocations(self.__row(location)):
             if len(self.sudoku[x]) != 1:
@@ -117,6 +162,9 @@ class SudokuSolver:
                 self.__assignInRow(location, x)
 
     def __assignCol(self, location):
+        """
+            (Private method) Assign all possibilities to the location in all columns
+        """
         sumCol = []
         for x in self.__colLocations(self.__col(location)):
             if len(self.sudoku[x]) != 1:
@@ -127,6 +175,9 @@ class SudokuSolver:
                 self.__assignInCol(location, x)
 
     def __assignSub(self, location):
+        """
+            (Private method) Assign all possibilities to the location in all subgrids
+        """
         sumSub = []
         for x in self.__subLocations(self.__sub(location)):
             if len(self.sudoku[x]) != 1:
@@ -137,12 +188,17 @@ class SudokuSolver:
                 self.__assignInSub(location, x)
 
     def __assign(self, location):
+        """
+            (Private method) Assign all possibilities to the location
+        """
         self.__assignRow(location)
         self.__assignCol(location)
         self.__assignSub(location)
 
     def solveOneStep(self):
-        # solve algorithm
+        """
+            Set one step towards solving the Sudoku
+        """
         for x in range(self.size ** 2):
             if len(self.sudoku[x]) == 1:
                 self.__removePossiblity(x, self.sudoku[x][0])
@@ -150,6 +206,9 @@ class SudokuSolver:
                 self.__assign(x)
 
     def solve(self, maxSteps = 20):
+        """
+            Solve the Sudoku by setting steps towards solving, until the maxSteps is reached. Default maxSteps = 20
+        """
         steps = 0
         while not self.isSolved():
             self.solveOneStep()
